b9c99301a93d9236d38a3027c4de4217b652b8ae
dejetem <enyeoluwarotimisimon@gmail.com>
2022-12-29 16:00:55 +0100

    rail

--- livechat/settings.py
+++ livechat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['livechat-production-9c6c.up.railway.app/']
+ALLOWED_HOSTS = ['livechat-production-9c6c.up.railway.app']
 
 
 # Application definition
